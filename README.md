069978fa2c938cde8439961e1db733d3826a87d3
nmrchvz <npchavez@ualberta.ca>
2021-09-21 15:30:31 -0600

    added deep.png & root.png

--- README.md
+++ README.md
@@ -24,3 +24,16 @@ examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
 
+Remarks
+========================
+Nomar Chavez
+npchavez
+1603039
+
+This is a forked repo for the purpose of making a webserver as a CMPUT 404 assignment.
+I declare that I have not collaborated with anyone on the completiong of this assignment
+Reference:
+* https://umbraco.com/knowledge-base/http-status-codes/
+* https://www.geeksforgeeks.org/python-os-path-isfile-method/
+* https://stackoverflow.com/questions/82831/how-do-i-check-whether-a-file-exists-without-exceptions
+* https://docs.python.org/3/library/socketserver.html
--- server.py
+++ server.py
@@ -28,34 +28,33 @@ import inspect
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
-# os.path - https://www.geeksforgeeks.org/os-path-module-python/
-# http status code - https://umbraco.com/knowledge-base/http-status-codes/
-# request.sendall () - https://docs.python.org/3/library/socketserver.html
-
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip().decode("utf-8")
         request_buffer = self.data.split()
         # Checks to see if the request is a GET request, otherwise return 405 Method not found
+        # https://umbraco.com/knowledge-base/http-status-codes/
         if (request_buffer[0].upper() == 'GET'):
             path = 'www' + request_buffer[1]
             # If the path is a file or directory attempt to parse its request and send it to the
             # webserver, otherwise return 404 Not Found
+            # https://www.geeksforgeeks.org/python-os-path-isfile-method/
             if os.path.isfile(path) or os.path.isdir(path) or os.path.exists(path):
                 ftype = path.split(".")[-1].lower()
-                # if path ends in '/', return index html from directories
-                
-                # if path does not end in '/' or css or html/htm return 301 Moved Permamently
+                # if path does not end in '/' or css or html/htm add /index.html
                 if path[-1] != "/" and ftype != 'css' and ftype != 'html' and ftype != "htm":
                     path += "/index.html"
+                    # if path exists, return 301 Moved Permanently
+                    # if the path doesn't exists, then 404 Not Found
+                    # https://stackoverflow.com/questions/82831/how-do-i-check-whether-a-file-exists-without-exceptions
                     if os.path.exists(path):
                         request_message = open(path).read()
                         self.message("301 Moved Permanently", "", request_message)
                     else:
-                        self.message("404 Not found")
+                        self.message("404 Not Found")
                     return
-
+                # if path does end in '/' then add index.html and send the message, otherwise send the message as is
                 if path[-1] == "/":
                     request_message = open(path + "index.html").read()
                     self.message("200 OK", 'html',request_message)
@@ -63,11 +62,17 @@ class MyWebServer(socketserver.BaseRequestHandler):
                     request_message = open(path).read()
                     self.message("200 OK", content_type = ftype, file = request_message)
             else:
-                self.message("404 Not found")
+                self.message("404 Not Found")
         else:
-            # Returns status code 405 Method Not Allowed (POST/PUT/DELETE)
-            self.message("405 Method  Method Not Allowed")
-    
+            self.message("405 Method Not Allowed")
+    '''
+    Purpose: To format a proper HTTP/1.1 messsage for request.sendall() 
+    Parameters: status_code (int) - a intger to identify status of a request
+                content_type (string) - a string to identify the content (css/html)
+                file (string) - the status message returned by the request
+    Return: None
+    References: https://docs.python.org/3/library/socketserver.html
+    '''
     def message(self, status_code, content_type = "", file = ""):
         message = "HTTP/1.1 " + status_code + "\r\n"
         if content_type:
